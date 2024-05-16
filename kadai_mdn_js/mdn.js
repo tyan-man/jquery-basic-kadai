b5f6e783f57cb22eff7713a16a5d675dc0622036
tyan-man <ringo.ri.1208@gmail.com>
2024-05-16 19:37:45 +0900

    mdn.jsを修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,8 @@
-const today = new Date();
+const date = new Date();
 const [month,day,year] = [
-    today.getMonth(),
-    today.getDate(),
-    today.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear(),
 ];
 
 console.log(year+"年",month+1+"月",day+"日");
