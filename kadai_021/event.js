866e2f373dd24b6844c0e9a26717b8d60669122f
tyan-man <ringo.ri.1208@gmail.com>
2024-06-02 16:55:15 +0900

    kadai_021を修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -2,7 +2,8 @@ const btn = document.getElementById('btn');
 
 const text = document.getElementById('text');
 
-
-setTimeout(btn.addEventListener('click', () => {
-    text.textContent = "ボタンをクリックしました";
-}, 2000));
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        text.textContent = "ボタンをクリックしました";
+    }, 2000);
+});
